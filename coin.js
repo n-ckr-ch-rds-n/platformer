78d17067c3b37c2191d37add200cc14d5a025076
Nick <nicholasjohnrichardson@gmail.com>
2018-08-20 17:56:06 +0100

    created Coin class

---
+++ coin.js
@@ -0,0 +1,16 @@
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() { return "coin"; }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(basePos, basePos, Math.random() * Math.Pi * 2);
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
