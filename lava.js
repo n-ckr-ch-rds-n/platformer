c33897e21582d0da3d0d895e127db5f873d49087
Nick <nicholasjohnrichardson@gmail.com>
2018-08-20 17:31:52 +0100

    created Lava class

---
+++ lava.js
@@ -0,0 +1,18 @@
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+  get type() { return "lava"; }
+
+  static create(pos, ch) {
+    if (ch == "=") {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch == "|") {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch == "v") {
+      return new Lava(pos, new Vec(0, 3), pos);
+    }
+  }
+}
