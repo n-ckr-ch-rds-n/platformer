5eb917e0ce9da1a2e1e6420481b68a7f73418a01
Nick <nicholasjohnrichardson@gmail.com>
2018-08-21 16:25:09 +0100

    added touches method to level class

--- js/level.js
+++ js/level.js
@@ -16,3 +16,19 @@ class Level {
     });
   }
 }
+
+Level.prototype.touches = function (pos, size, type) {
+    var xStart = Math.floor(pos.x);
+    var xEnd = Math.ceil(pos.x + size.x);
+    var yStart = Math.floor(pos.y);
+    var yEnd = Math.ceil(pos.y + size.y);
+
+    for (var y = yStart; y < yEnd; y++) {
+      for (var x = xStart; x < xEnd; x++) {
+        let isOutside = x < 0 || x >= this.width ||
+                        y < 0 || y >= this.height;
+        let here = isOutside ? "wall" : this.rows[y][x];
+        if (here == type) return true;
+      }
+    }
+};
